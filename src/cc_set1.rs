4971d33e009a0c13d79312707f2388641cdf2f7d
Frank Meerkoetter <frank@meerkoetter.org>
2014-11-26 10:40:29 +0100

    Solve the 2nd exercise of the 1st set of challenges

--- src/cc_set1.rs
+++ src/cc_set1.rs
@@ -63,7 +63,7 @@ fn hex2bin(hex: &str) -> Result<Vec<u8>, String> {
 fn set1_exercise1() {
     let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let expected_output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-    
+
     let bin = hex2bin(input);
     match bin {
         Ok(bin) => assert!(to_base64(bin).as_slice() == expected_output),
@@ -71,6 +71,46 @@ fn set1_exercise1() {
     }
 }
 
+fn xor(lhs : Vec<u8>, rhs : Vec<u8>) -> Vec<u8> {
+    let mut it = lhs.iter().zip(rhs.iter());
+    let mut result = Vec::new();
+    for p in it {
+        let (l, r) = p;
+        result.push(*l ^ *r);
+    }
+    result
+}
+
+fn bin2hex(bin: Vec<u8>) -> String {
+    let mut result = String::new();
+    for b in bin.iter() {
+        let hex = format!("{:x}", *b);
+        result.push(hex.as_bytes()[0] as char);
+        result.push(hex.as_bytes()[1] as char);
+    }
+    result
+}
+
+fn set1_exercise2() {
+    let lhs = "1c0111001f010100061a024b53535009181c";
+    let lhs_bin = hex2bin(lhs);
+    if lhs_bin.is_err() {
+        println!("{}", lhs_bin);
+        return;
+    }
+
+    let rhs = "686974207468652062756c6c277320657965";
+    let rhs_bin = hex2bin(rhs);
+    if rhs_bin.is_err() {
+        println!("{}", lhs_bin);
+        return;
+    }
+
+    assert!(bin2hex(xor(lhs_bin.ok().unwrap(), rhs_bin.ok().unwrap())).as_slice()
+        == "746865206b696420646f6e277420706c6179");
+}
+
 fn main() {
     set1_exercise1();
+    set1_exercise2();
 }
